757042de10b9cb3161b27369f86b27b5e0b6dd30
ZHDKk <1083861095@qq.com>
2017-11-09 10:41:25 +0800

    第一次提交

--- UITableView/ViewController.h
+++ UITableView/ViewController.h
@@ -8,7 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITableViewDelegate,UITableViewDataSource>
+{
+    //定义一个数据视图对象，用来显示大量相同格式的数据
+    UITableView *_tableView;
+}
 
 
 @end
--- UITableView/ViewController.m
+++ UITableView/ViewController.m
@@ -17,6 +17,43 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //创建数据视图
+    _tableView = [[UITableView alloc] initWithFrame:self.view.bounds  style:UITableViewStyleGrouped];
+    //设置数据视图的代理对象
+    _tableView.delegate = self;
+    //设置数据视图的数据源对象
+    _tableView.dataSource = self;
+    
+    [self.view addSubview:_tableView];
+    
+}
+
+//获取每组元素的个数，必须要实现的协议函数
+-(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 5;
+}
+//设置数据视图的组数
+-(NSInteger) numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 3;
+}
+
+//创建单元格对象函数
+-(UITableViewCell*) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *cellStr = @"cell";
+    UITableViewCell* cell= [_tableView dequeueReusableCellWithIdentifier:cellStr];
+    
+    if (cell == nil) {
+        //创建一个单元格对象  参数一：UITableViewCellStyleDefault:单元格样式。 参数二：单元格复用标记
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellStr];
+    }
+    
+    NSString *str=[NSString stringWithFormat:@"第%ld组,第%ld行",indexPath.section,indexPath.row];
+    cell.textLabel.text = str;
+    return cell;
 }
 
 
